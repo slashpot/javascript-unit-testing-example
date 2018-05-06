2fd5466acc9013ab29c66d8c70a47bbffc3ae53f
Eric Lin <ttttt123465@gmail.com>
2018-05-06 17:09:12 +0800

    finish example

---
+++ Example/JS Example/src/person.js
@@ -0,0 +1,17 @@
+﻿var Person = (function () {
+    function Person() {
+    }
+    Person.prototype.setFirstName = function (value) {
+        this.firstName = value;
+    };
+    Person.prototype.setLastName = function (value) {
+        this.lastName = value;
+    };
+    Person.prototype.getFullName = function (lastNameFirst) {
+        if (lastNameFirst === true) {
+            return this.lastName + ", " + this.firstName;
+        }
+        return this.firstName + ", " + this.lastName;
+    };
+    return Person;
+}());
---
+++ Example/JS Example/tests/test.js
@@ -0,0 +1,27 @@
+﻿describe("Person FullName", function () {
+
+    var person;
+
+    beforeEach(function() {
+        person = new Person();
+        person.setFirstName("Joe");
+        person.setLastName("Smith");
+    });
+
+    it("should concatenate first and last names", function () {
+        expect(person.getFullName(false)).toBe("Joe, Smith");
+    });
+
+    it("should concatenate first and last names - incorrect", function () {
+        expect(person.getFullName(false)).not.toBe("Joe, Doe");
+    });
+
+    it("should concatenate lastname first", function () {
+        expect(person.getFullName(true)).toBe("Smith, Joe");
+    });
+
+    it("should not concatinate firstname first", function () {
+        expect(person.getFullName(true)).not.toBe("Joe, Smith");
+    });
+});
+
