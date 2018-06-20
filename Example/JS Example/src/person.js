9000c5db60142b3fe18c984906e87db8c9c05f80
Eric Lin <ttttt123465@gmail.com>
2018-06-21 01:05:39 +0800

    add example

--- Example/JS Example/src/person.js
+++ Example/JS Example/src/person.js
@@ -1,17 +1,37 @@
-﻿var Person = (function () {
-    function Person() {
+﻿//var Person = (function () {
+//    function Person() {
+//    }
+//    Person.prototype.setFirstName = function (value) {
+//        this.firstName = value;
+//    };
+//    Person.prototype.setLastName = function (value) {
+//        this.lastName = value;
+//    };
+//    Person.prototype.getFullName = function (lastNameFirst) {
+//        if (lastNameFirst === true) {
+//            return this.lastName + ", " + this.firstName;
+//        }
+//        return this.firstName + ", " + this.lastName;
+//    };
+//    return Person;
+//}());
+
+class Person {
+    constructor() {
+
     }
-    Person.prototype.setFirstName = function (value) {
+    setFirstName(value) {
         this.firstName = value;
-    };
-    Person.prototype.setLastName = function (value) {
+    }
+
+    setLastName(value) {
         this.lastName = value;
-    };
-    Person.prototype.getFullName = function (lastNameFirst) {
-        if (lastNameFirst === true) {
+    }
+
+    getFullName(lastNameFirst) {
+        if (lastNameFirst) {
             return this.lastName + ", " + this.firstName;
         }
         return this.firstName + ", " + this.lastName;
-    };
-    return Person;
-}());
+    }
+}
